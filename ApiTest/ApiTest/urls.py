101846b156e47460ebd79cd40c3a9ac18386c874
chenzhibin <chenzhibin@inke.cn>
2021-02-18 17:51:03 +0800

    新增31,32,33部分代码

--- ApiTest/ApiTest/urls.py
+++ ApiTest/ApiTest/urls.py
@@ -37,5 +37,9 @@ urlpatterns = [
     url(r"^cases/(?P<id>.*)/$",open_cases),#进入用例设置
     url(r"^project_set/(?P<id>.*)/$",open_project_set),#进入项目设置
     url(r"^save_project_set/(?P<id>.*)/$",save_project_set),#进入项目设置
+    url(r"^project_api_add/(?P<Pid>.*)/$",project_api_add),#新增接口
+    url(r"^project_api_del/(?P<id>.*)/$",project_api_del),#删除接口
+    url(r"^save_bz/$", save_bz),#保存备注
+    url(r"^get_bz/$", get_bz), # 获取备注
 
 ]
