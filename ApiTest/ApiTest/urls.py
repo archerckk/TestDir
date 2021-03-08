bf15921484ea739b27b74df1d2ca9b7bdf05bf5a
archerckk@163.com <archerckk@163.com>
2021-03-08 08:45:15 +0800

    提交平台48,49，复制api接口，异常测试代码

--- ApiTest/ApiTest/urls.py
+++ ApiTest/ApiTest/urls.py
@@ -41,8 +41,9 @@ urlpatterns = [
     url(r"^project_api_del/(?P<id>.*)/$",project_api_del),#删除接口
     url(r"^save_bz/$", save_bz),#保存备注
     url(r"^get_bz/$", get_bz), # 获取备注
-    url(r"^Api_save/$",Api_save),
-    url(r"^get_api_data/$",get_api_data),
-    url(r"^Api_send/$",Api_send),
+    url(r"^Api_save/$",Api_save), #保存接口
+    url(r"^get_api_data/$",get_api_data), #获取api数据
+    url(r"^Api_send/$",Api_send), #发送接口请求
+    url(r"^copy_api/$",copy_api), #复制接口
 
 ]
